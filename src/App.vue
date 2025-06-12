a6d6380222aff3d11abc6d9020b7227e75cf0bfe
MiipisSus <kokurui0113@gmail.com>
2025-06-12 19:25:12 +0800

    feat: Add App.vue to display all pages.

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Header from "./components/Header.vue";
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col">
+    <Header />
+    <main class="bg-gray-100 flex flex-1">
+      <router-view class="w-full" />
+    </main>
+  </div>
+</template>
+
+<style scoped></style>
