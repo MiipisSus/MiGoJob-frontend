9ac05efbb91fd88beebfd59d66c468c23cf3c893
MiipisSus <kokurui0113@gmail.com>
2025-06-12 19:25:42 +0800

    Add Home.vue.

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import SearchBar from "@/components/SearchBar.vue";
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center gap-6">
+    <span>
+      <p class="text-3xl">尋找下一份工作...</p>
+    </span>
+    <SearchBar />
+  </div>
+</template>
