b03357f52a273f6fd4010ac3f98b0018aadb4574
MiipisSus <kokurui0113@gmail.com>
2025-06-12 19:21:31 +0800

    feat: Add PrimeVue to the app. feat: Add pinia to the app. feat: Add a router to the app to manage pages.

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";
+
+import "./style.css";
+import App from "./App.vue";
+import router from "./router";
+
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(router);
+app.use(pinia);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
+
+app.mount("#app");
---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
+import SearchPage from "@/views/SearchPage.vue";
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/search", component: SearchPage },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
