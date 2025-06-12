6eed1cbb1d28d93c220b17385c9499e785e26b98
MiipisSus <kokurui0113@gmail.com>
2025-06-12 19:26:04 +0800

    feat: Add SearchBar.vue as a component.

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import InputText from "primevue/inputtext";
+import Button from "primevue/button";
+
+import useJobStore from "@/stores/job";
+
+const router = useRouter();
+const jobStore = useJobStore();
+
+const handleSearch = async () => {
+  if (router.currentRoute.value.path !== "/search") {
+    router.push("/search");
+  }
+};
+</script>
+
+<template>
+  <div class="flex place-content-center w-3/5 h-13 gap-3">
+    <InputText
+      v-model="jobStore.keyword"
+      class="w-3/5"
+      placeholder="關鍵字（如:全端工程師）"
+    />
+    <Button
+      type="button"
+      label="搜尋"
+      icon="pi pi-search"
+      @click="handleSearch"
+    />
+  </div>
+</template>
