6d90d40d29a5d01e276ba8c4d7673c193dffb21e
MiipisSus <kokurui0113@gmail.com>
2025-06-13 17:27:30 +0800

    feat: Update home page style.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,12 @@ import SearchBar from "@/components/SearchBar.vue";
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center gap-6">
-    <span>
-      <p class="text-3xl">尋找下一份工作...</p>
-    </span>
+  <div
+    class="flex flex-col items-center justify-center gap-6 bg-gradient-to-b from-third to-secondary-100"
+  >
+    <p>
+      <span class="text-3xl font-bold">尋找下一份工作...</span>
+    </p>
     <SearchBar />
   </div>
 </template>
