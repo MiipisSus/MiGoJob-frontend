7db2c63f5f52b65fa930a22f6106d7ee53e0c3b6
MiipisSus <kokurui0113@gmail.com>
2025-06-13 17:28:00 +0800

    feat: Add nav router component to SearchPage.

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
-import DataView from "primevue/dataview";
 
 import useJobStore from "@/stores/job";
 import SearchBar from "@/components/SearchBar.vue";
+import JobDataView from "@/components/JobDataView.vue";
+import BreadcrumbRouter from "@/components/BreadcrumbRouter.vue";
 
 const jobStore = useJobStore();
 
@@ -13,43 +14,12 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="flex flex-col p-15 gap-10">
+  <div class="flex flex-col pt-5 gap-8">
+    <BreadcrumbRouter class="w-4/5 mx-auto" />
     <div class="flex place-content-center">
       <SearchBar class="w-full" />
     </div>
-    <DataView
-      :value="jobStore.jobs"
-      class="!rounded-md overflow-hidden shadow-md"
-      paginator
-      :rows="6"
-    >
-      <template #list="slotProps">
-        <div class="flex flex-col overflow-auto">
-          <li v-for="(job, index) in slotProps.items" :key="job.id">
-            <div class="flex m-5 h-30">
-              <div class="">
-                <p class="font-bold text-lg">{{ job.name }}</p>
-                <a
-                  class="text-sm hover:text-emerald-500 transition duration-200"
-                  :href="`/company/${job.company_id}`"
-                >
-                  {{ job.company_name }}
-                </a>
-              </div>
-              <div class="ml-auto">
-                <p>
-                  月薪 NT. {{ job.salary_min }} ~ NT. {{ job.salary_max }} 元
-                </p>
-              </div>
-            </div>
-            <hr
-              class="border-t border-gray-300 mx-5"
-              v-if="index !== slotProps.items.length - 1"
-            />
-          </li>
-        </div>
-      </template>
-    </DataView>
+    <JobDataView />
   </div>
 </template>
 
