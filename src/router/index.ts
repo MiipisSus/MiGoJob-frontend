60e82cbf0245ae0ff154c4b488af3d6c0c190c5c
MiipisSus <kokurui0113@gmail.com>
2025-06-13 17:24:31 +0800

    feat: Add `CompanyDetail` and `JobDetail` routes.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
 import SearchPage from "@/views/SearchPage.vue";
+import CompanyDetail from "@/views/CompanyDetail.vue";
+import JobDetail from "@/views/JobDetail.vue";
 
 const routes = [
-  { path: "/", component: Home },
-  { path: "/search", component: SearchPage },
+  { path: "/", component: Home, name: "Home" },
+  { path: "/search", component: SearchPage, name: "Search" },
+  {
+    path: "/company/:id",
+    component: CompanyDetail,
+    props: true,
+    name: "CompanyDetail",
+  },
+  { path: "/job/:id", component: JobDetail, props: true, name: "JobDetail" },
 ];
 
 const router = createRouter({
