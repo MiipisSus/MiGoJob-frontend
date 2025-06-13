9502c309059b6ad6d9720c7eccd7afcdb753c8a8
MiipisSus <kokurui0113@gmail.com>
2025-06-13 17:37:03 +0800

    chore: Delete uneeded style.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <header class="bg-white text-black p-4 flex justify-center">
+  <header class="bg-white p-4 flex justify-center">
     <a href="/"><img src="@/assets/header_icon.png" class="h-10" /></a>
   </header>
 </template>
